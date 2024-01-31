866e2203f6e1f24a5bb14a6ae4f8d7c89f03eb52
Suryansh Yadav <suryanshyadav159@gmail.com>
2024-01-31 23:18:14 +0530

    updated the success and cancel url

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -22,8 +22,8 @@ const payment= async (req, res) => {
       payment_method_types: ['card'],
       line_items:lineItems,
       mode:'payment',
-      success_url: `http://localhost:4000/payment/success?session_id={CHECKOUT_SESSION_ID}&propertyId=${propertyId}&userId=${userId}`,
-      cancel_url: `http://localhost:5173/cancel`,
+      success_url: `https://backendestate.onrender.com/payment/success?session_id={CHECKOUT_SESSION_ID}&propertyId=${propertyId}&userId=${userId}`,
+      cancel_url: `https://65b938fcdb5b472037ff67c6--lucky-manatee-19cd99.netlify.app/cancel`,
     });
     res.status(200).json(session.url);
   };
